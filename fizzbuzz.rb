363e477d1b11379b669847d1e821d2f395b94725
Leticia Molina <letimolina@gmail.com>
2018-04-21 19:53:49 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,11 @@
+def fizzbuzz(num)
+  if num / 3 == 0
+    puts "Fizz"
+  elsif num / 5 == 0
+    puts "Buzz"
+  elsif num / 3 == 0 && num / 5 == 0
+    puts "FizzBuzz"
+  else 
+    puts nil
+  end
+end
