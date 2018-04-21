b675366dbde333afb23dde5e1261c95342fa1eaf
Leticia Molina <letimolina@gmail.com>
2018-04-21 20:02:41 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(num)
-  if num % 3 == 0
-    puts "Fizz"
+  if num % 3 == 0 && num / 5 == 0
+    puts "FizzBuzz"
   elsif num % 5 == 0
     puts "Buzz"
-  elsif num % 3 == 0 && num / 5 == 0
-    puts "FizzBuzz"
+  elsif num % 3 == 0
+    puts "Fizz"
   else 
     puts nil
   end
